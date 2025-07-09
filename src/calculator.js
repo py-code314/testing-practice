cd94c7030e70f832d201dac9eb72effec473112d
srikk <122169588+py-code314@users.noreply.github.com>
2025-07-09 15:37:32 -0400

    feat: Add function & test to subtract two numbers
    - Create function `subtract` that returns the result of subtracting
    two numbers.
    - Add unit tests in `subtract.test.js` to evaluate the results of
    `subtract` function when given different arguments. The arguments
    could be positive numbers, negative numbers, zero,
    floating point numbers, non-numeric values or missing arguments.

--- src/calculator.js
+++ src/calculator.js
@@ -1,4 +1,5 @@
 const calculator = {
+  // Function to add two numbers
   add(x, y) {
     // Check for type
     if (typeof x !== 'number' || typeof y !== 'number') {
@@ -6,6 +7,15 @@ const calculator = {
     }
 
     return x + y
+  },
+  // Function to subtract two numbers
+  subtract(x, y) {
+    // Check for type
+    if (typeof x !== 'number' || typeof y !== 'number') {
+      throw new Error('Enter a number')
+    }
+
+    return x - y
   },
 }
 
--- tests/calculator.test.js
+++ tests/calculator.test.js
@@ -2,7 +2,7 @@
 // Import module
 const calculator = require('../src/calculator')
 
-// Tests
+// Tests for 'add' function
 describe('addNumbers', () => {
   test('adds two positive numbers', () => {
     expect(calculator.add(2, 3)).toEqual(5)
@@ -41,5 +41,59 @@ describe('addNumbers', () => {
   test('checks for missing arguments', () => {
     expect(() => calculator.add(5)).toThrow(Error)
     expect(() => calculator.add()).toThrow(Error)
+  })
+})
+
+// Tests for 'subtract' function
+describe('subtractNumbers', () => {
+  test('subtracts two positive numbers', () => {
+    expect(calculator.subtract(3, 3)).toEqual(0)
+    expect(calculator.subtract(3, 2)).toEqual(1)
+    expect(calculator.subtract(5, 6)).toEqual(-1)
+    expect(calculator.subtract(0, 0)).toEqual(0)
+  })
+
+  test('subtracts one positive, one negative number', () => {
+    expect(calculator.subtract(5, -6)).toEqual(11)
+    expect(calculator.subtract(-6, 5)).toEqual(-11)
+    expect(calculator.subtract(-5, -6)).toEqual(1)
+    expect(calculator.subtract(-0, 0)).toEqual(-0)
+  })
+
+  test('subtracts two negative numbers', () => {
+    expect(calculator.subtract(-5, -6)).toEqual(1)
+  })
+
+  test('subtracts a positive or negative number from zero', () => {
+    expect(calculator.subtract(0, -5)).toEqual(5)
+    expect(calculator.subtract(0, 5)).toEqual(-5)
+  })
+
+  test('subtracts zero from a positive or negative number', () => {
+    expect(calculator.subtract(5, 0)).toEqual(5)
+    expect(calculator.subtract(-5, 0)).toEqual(-5)
+  })
+
+  test('subtracts two floating point numbers', () => {
+    expect(calculator.subtract(0.1, 0.2)).toBeCloseTo(-0.1)
+    expect(calculator.subtract(-0.5, -0.6)).toBeCloseTo(0.1)
+    expect(calculator.subtract(-0.5, 0.6)).toBeCloseTo(-1.1)
+    expect(calculator.subtract(0.6, -0.5)).toBeCloseTo(1.1)
+  })
+
+  test('subtracts a zero from a floating point number', () => {
+    expect(calculator.subtract(0, 0.2)).toEqual(-0.2)
+    expect(calculator.subtract(0, -2.6)).toEqual(2.6)
+  })
+
+  test('checks for numbers as both arguments', () => {
+    expect(() => calculator.subtract('hello', 5)).toThrow(Error)
+    expect(() => calculator.subtract(5, null)).toThrow(Error)
+    expect(() => calculator.subtract('hello', NaN)).toThrow(Error)
+  })
+
+  test('checks for missing arguments', () => {
+    expect(() => calculator.subtract(5)).toThrow(Error)
+    expect(() => calculator.subtract()).toThrow(Error)
   })
 })
