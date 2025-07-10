bdc5ad87bcf927e6777f833d51dee07a7e0a9e79
srikk <122169588+py-code314@users.noreply.github.com>
2025-07-09 21:35:10 -0400

    feat: Add function & tests to divide two numbers
    - Create function `divide` that returns the result of division of
    two numbers.
    - Write unit tests to test `divide` function. Make sure to test with
    different types of numbers including positive, negative, and
    floating point numbers, and zero.

--- src/calculator.js
+++ src/calculator.js
@@ -17,6 +17,19 @@ const calculator = {
 
     return x - y
   },
+  // Function to divide two numbers
+  divide(x, y) {
+    // Check for type
+    if (typeof x !== 'number' || typeof y !== 'number') {
+      throw new Error('Enter a number')
+    }
+    // Throw zero division error
+    if (y === 0) {
+      throw new Error('Zero division error')
+    }
+
+    return x / y
+  },
 }
 
 module.exports = calculator
--- tests/calculator.test.js
+++ tests/calculator.test.js
@@ -97,3 +97,58 @@ describe('subtractNumbers', () => {
     expect(() => calculator.subtract()).toThrow(Error)
   })
 })
+
+// Tests for 'divide' function
+describe('divideNumbers', () => {
+  test('divides two positive numbers', () => {
+    expect(calculator.divide(3, 3)).toEqual(1)
+    expect(calculator.divide(3, 2)).toEqual(1.5)
+    expect(calculator.divide(5, 6)).toBeCloseTo(0.83)
+  })
+
+  test('divides two negative numbers', () => {
+    expect(calculator.divide(-3, -3)).toEqual(1)
+    expect(calculator.divide(-3, -2)).toEqual(1.5)
+    expect(calculator.divide(-5, -6)).toBeCloseTo(0.83)
+  })
+
+  test('divide a number by zero', () => {
+    expect(() => calculator.divide(0, 0)).toThrow(Error)
+    expect(() => calculator.divide(2, 0)).toThrow(Error)
+    expect(() => calculator.divide(-2, 0)).toThrow(Error)
+  })
+
+  test('divides zero by a number', () => {
+    expect(calculator.divide(0, 3)).toEqual(0)
+    expect(calculator.divide(0, -2)).toEqual(-0)
+    expect(calculator.divide(0, 2.5)).toEqual(0)
+  })
+
+  test('divides one positive, one negative number', () => {
+    expect(calculator.divide(5, -6)).toBeCloseTo(-0.83)
+    expect(calculator.divide(-6, 5)).toEqual(-1.2)
+  })
+
+  test('handles floating point division', () => {
+    expect(calculator.divide(5.5, 2.2)).toBeCloseTo(2.5)
+    expect(calculator.divide(5, 2.5)).toBeCloseTo(2)
+  })
+
+  test('divides two floating point numbers', () => {
+    expect(calculator.divide(0.1, 0.3)).toBeCloseTo(0.33)
+    expect(calculator.divide(-0.5, -0.6)).toBeCloseTo(0.83)
+    expect(calculator.divide(-0.5, 0.6)).toBeCloseTo(-0.83)
+    expect(calculator.divide(0.6, -0.5)).toBeCloseTo(-1.2)
+  })
+
+  test('checks for numbers as both arguments', () => {
+    expect(() => calculator.divide('hello', 5)).toThrow(Error)
+    expect(() => calculator.divide(5, null)).toThrow(Error)
+    expect(() => calculator.divide('hello', NaN)).toThrow(Error)
+  })
+
+  test('checks for missing arguments', () => {
+    expect(() => calculator.divide(5)).toThrow(Error)
+    expect(() => calculator.divide()).toThrow(Error)
+  })
+})
