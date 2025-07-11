e15120fd61c911b3d2c760012561ec4d1128e03b
srikk <122169588+py-code314@users.noreply.github.com>
2025-07-11 14:46:25 -0400

    test: Add tests to check for negative values in array
    Array might have a few negative values or all negative values.

--- tests/analyze-array.test.js
+++ tests/analyze-array.test.js
@@ -4,7 +4,25 @@ const analyzeArray = require('../src/analyze-array')
 
 // Tests
 describe('analyzeArray', () => {
-  test('outputs an array', () => {
+  test('outputs an object from an array', () => {
     expect(analyzeArray([1,8,3,4,2,6])).toEqual({average: 4, min: 1, max: 8, length: 6})
+    expect(analyzeArray([11,80,33,46])).toEqual({average: 42.5, min: 11, max: 80, length: 4})
+  })
+
+  test('array has few negative values', () => {
+    const result = analyzeArray([1, -8, 3, 4, -2, 6])
+    expect(result.average).toBeCloseTo(0.666, 2)
+    expect(result.min).toEqual(-8)
+    expect(result.max).toEqual(6)
+    expect(result.length).toEqual(6)
+  })
+
+  test('array has all negative values', () => {
+    expect(analyzeArray([-1, -8, -3, -4, -2, -6])).toEqual({
+      average: -4,
+      min: -8,
+      max: -1,
+      length: 6,
+    })
   })
 })
