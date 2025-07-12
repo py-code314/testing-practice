95374bf59f4265d9d3fe489a1569a8dd39c44e0c
srikk <122169588+py-code314@users.noreply.github.com>
2025-07-11 21:32:03 -0400

    refactor: Change syntax from 'test' to 'it'

--- __tests__/reverse-string.test.js
+++ __tests__/reverse-string.test.js
@@ -2,41 +2,43 @@
 // Import module
 const reverseString = require('../src/reverse-string')
 
+//TODO: change test to it
+//TODO: add test to check for a string
 // Tests
 describe('reverseString', () => {
-  test('reverses a single word', () => {
+  it('reverses a single word', () => {
     expect(reverseString('hello')).toEqual('olleh')
   })
 
-  test('reverses a string with spaces', () => {
+  it('reverses a string with spaces', () => {
     expect(reverseString('Good Morning All')).toEqual('llA gninroM dooG')
   })
 
-  test('reverses a single character', () => {
+  it('reverses a single character', () => {
     expect(reverseString('a')).toEqual('a')
   })
 
-  test('reverses a palindrome', () => {
+  it('reverses a palindrome', () => {
     expect(reverseString('malayalam')).toEqual('malayalam')
   })
 
-  test('reverses a string with emojis', () => {
+  it('reverses a string with emojis', () => {
     expect(reverseString('😊😂🎉')).toEqual('🎉😂😊')
   })
 
-  test('reverses a string with accents', () => {
+  it('reverses a string with accents', () => {
     expect(reverseString('Éclairs')).toEqual('srialcÉ')
   })
 
-  test('reverses a string with numbers and punctuation', () => {
+  it('reverses a string with numbers and punctuation', () => {
     expect(reverseString("!T'S a v*st #OC3AN!")).toEqual("!NA3CO# ts*v a S'T!")
   })
 
-  test('works with empty string', () => {
+  it('works with empty string', () => {
     expect(reverseString('')).toEqual('')
   })
 
-  test('works with spaces', () => {
+  it('works with white spaces', () => {
     expect(reverseString('   ')).toEqual('   ')
   })
 })
